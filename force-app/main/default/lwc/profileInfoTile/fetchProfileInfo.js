6500cb18012c94f14a0a6d0f1c6377d44dbc70b5
gruiz90 <germanruizravi@gmail.com>
2020-11-06 13:57:18 -0300

    New lwc for profile info tile

---
+++ force-app/main/default/lwc/profileInfoTile/fetchProfileInfo.js
@@ -0,0 +1,48 @@
+const query = `
+    query profileInfo {
+        me {
+            person {
+                id
+                firstName            
+                lastName
+                jobTitle
+                avatar
+                __typename
+            }
+        }
+    }
+`;
+
+const TOKEN = "SECRET_TOKEN";
+
+export default function fetchProfileInfo() {
+  const graphql = JSON.stringify({
+    graphql: query,
+    variables: {}
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      Authorization: `Bearer ${TOKEN}`
+    },
+    body: graphql,
+    redirect: "follow"
+  };
+
+  // try {
+  //     const response = await fetch("https://api.philanthropycloud.com.qa3.makanaplatform.com/graphql", requestOptions);
+  //     if (response.ok) {
+  //         return response.json
+  //     }
+  // } catch (error) {
+  //     console.log('error', error)
+  // }
+  return fetch(
+    "https://api.philanthropycloud.com.qa3.makanaplatform.com/graphql",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .catch((error) => console.log("error", error));
+}
---
+++ force-app/main/default/lwc/profileInfoTile/profileInfoTile.js
@@ -0,0 +1,26 @@
+import { LightningElement } from "lwc";
+import fetchProfileInfo from "./fetchProfileInfo";
+
+export default class ProfileInfoTile extends LightningElement {
+  profile = {
+    name: "John Doe",
+    jobTitle: "CEO",
+    avatar: ""
+  };
+  error = false;
+
+  async connectedCallback() {
+    const response = await fetchProfileInfo();
+    if (response.data) {
+      this.profile = {
+        name: `${response.data.me.person.firstName} ${response.data.me.person.firstName}`,
+        jobTitle: response.data.me.person.jobTitle,
+        avatar: response.data.me.person.avatar
+      };
+    } else {
+      this.error = true;
+      console.log(`Error: ${response.error}`);
+      console.log(`Message: ${response.message}`);
+    }
+  }
+}
